b0b6f03173998ecc90ee15cf7216faf8d581cba0
wilsondev94 <ohioleomole@gmail.com>
2025-01-31 02:13:38 +0000

    Minor change

--- src/store/darkModeStore.ts
+++ src/store/darkModeStore.ts
@@ -1,11 +1,25 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface DarkModeState {
   isDarkMode: boolean;
   setIsDarkMode: (darkMode: boolean) => void;
 }
 
-export const useDarkModeStore = create<DarkModeState>((set) => ({
-  isDarkMode: false,
-  setIsDarkMode: (darkMode) => set({ isDarkMode: darkMode }),
-}));
+// export const useDarkModeStore = create<DarkModeState>((set) => ({
+//   isDarkMode: false,
+//   setIsDarkMode: (darkMode) => set({ isDarkMode: darkMode }),
+// }));
+
+export const useDarkModeStore = create<DarkModeState>()(
+  persist(
+    (set, get) => ({
+      isDarkMode: false, // Default to light mode
+      setIsDarkMode: () => set({ isDarkMode: !get().isDarkMode }),
+    }),
+    {
+      name: "darkMode-storage", // Key for localStorage
+      storage: createJSONStorage(() => localStorage), // Persist in localStorage
+    }
+  )
+);
